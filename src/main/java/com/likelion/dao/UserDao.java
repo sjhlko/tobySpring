7f2e4895e7b9d0541cedb5d9cc0e2c847181f337
서정희 <sjhlko@naver.com>
2022-10-21 09:23:44 +0900

    getcount, deleteAll추가

--- src/main/java/com/likelion/dao/UserDao.java
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -2,6 +2,7 @@ package com.likelion.dao;
 
 
 import com.likelion.domain.User;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.sql.*;
 import java.util.Map;
@@ -40,6 +41,75 @@ public class UserDao {
         }
     }
 
+    public void deleteAll() throws SQLException{
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = cm.makeConnection();
+            ps = c.prepareStatement("delete from users");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally { //에러가 나도 실행됨 -> 에러가 나도 close는 해야함
+            if(ps!=null){
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if(c!=null){
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        ps.executeUpdate();
+        ps.close();
+        c.close();
+
+    }
+
+    public int getCount() throws SQLException{
+        ResultSet rs = null;
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = cm.makeConnection();
+            ps = c.prepareStatement("select count(*) from users");
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(rs!=null){
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+            if(ps!=null){
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if(c!=null){
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+    }
+
     public User findById(String id) {
         Map<String, String> env = System.getenv();
         Connection c;
@@ -53,13 +123,18 @@ public class UserDao {
 
             // Query문 실행
             ResultSet rs = pstmt.executeQuery();
-            rs.next();
-            User user = new User(rs.getString("id"), rs.getString("name"),
-                    rs.getString("password"));
+            User user = null;
+            if(rs.next()){
+                user = new User(rs.getString("id"), rs.getString("name"),
+                        rs.getString("password"));
+
+            }
 
             rs.close();
             pstmt.close();
             c.close();
+
+            if(user == null) throw new EmptyResultDataAccessException(1);
 
             return user;
 
--- src/test/java/com/likelion/dao/UserDaoTest.java
+++ src/test/java/com/likelion/dao/UserDaoTest.java
@@ -1,30 +1,75 @@
 package com.likelion.dao;
 
 import com.likelion.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(SpringExtension.class) //스프링에만 있는 기능을 junit에서도 쓸 수있다.
 @ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
 
-    @Autowired
+
+    @Autowired //쓰려면 @ExtendWith(SpringExtension.class)이 필요함
     ApplicationContext context;
+    UserDao userDao;
+    User user1;
+    User user2;
+    User user3;
+
+    @BeforeEach
+    void setUp(){
+        this.userDao = context.getBean("awsUserDao", UserDao.class);
+        user1 = new User("1","서정희", "1234");
+        user2 = new User("2","서정희2", "1234");
+        user3 = new User("3","서정희3", "1234");
+
+    }
 
     @Test
-    void addAndGet() {
-        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
-        String id = "29";
-        userDao.add(new User(id, "EternityHwan", "1234"));
-        User user = userDao.findById(id);
+    void addAndGet() throws SQLException {
+        User user1 = new User("1","서정희", "1234");
 
-        assertEquals("EternityHwan", user.getName());
+        userDao.deleteAll();
+        assertEquals(0,userDao.getCount());
+
+        String id = "29";
+        userDao.add(user1);
+        assertEquals(1,userDao.getCount());
+        User user = userDao.findById(user1.getId());
+
+        assertEquals(user1.getName(), user.getName());
+        //user1.getName()은 메소드내에서 선언되어있는 값 user는 디비에서 가져온 값
         assertEquals("1234", user.getPassword());
+    }
+
+    @Test
+    void count() throws SQLException{
+
+        userDao.deleteAll();
+        assertEquals(0,userDao.getCount());
+        userDao.add(user1);
+        assertEquals(1,userDao.getCount());
+        userDao.add(user2);
+        assertEquals(2,userDao.getCount());
+        userDao.add(user3);
+        assertEquals(3,userDao.getCount());
+    }
+
+    @Test
+    void findById(){
+        assertThrows(EmptyResultDataAccessException.class,()->{
+            userDao.findById("30");
+        });
+
     }
 }
